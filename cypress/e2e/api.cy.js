6346a4e1af81bb06e84687655f8107935d03cb97
martica0818-roj18 <martica0818@gmail.com>
2026-03-26 23:08:27 -0500

    Update api.cy.js

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,1 +1,8 @@
-
+describe('API Test', () => {
+  it('GET users', () => {
+    cy.request('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        expect(response.status).to.eq(200)
+      })
+  })
+})
